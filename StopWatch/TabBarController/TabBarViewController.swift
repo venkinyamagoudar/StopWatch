8121f2fc3c0ccebb2996e37f0212bfaa97215fd7
Venkatesh Nyamagoudar <vnyamagoudar@gmail.com>
2023-03-23 19:15:07 -0400

    Changes after timer implementation

--- StopWatch/TabBarController/TabBarViewController.swift
+++ StopWatch/TabBarController/TabBarViewController.swift
@@ -10,11 +10,13 @@ import UIKit
 class TabBarViewController: UITabBarController {
 
     let stopWatchCoordinator = StopWatchCoordinator(navigationController: UINavigationController())
+    let alarmWatchCoordinator = AlarmCoordinator(navigationController: UINavigationController())
     let timerWatchCoordinator = TimerCoordinator(navigationController: UINavigationController())
     
     override func viewDidLoad() {
         super.viewDidLoad()
         stopWatchCoordinator.start()
+        alarmWatchCoordinator.start()
         timerWatchCoordinator.start()
         
         self.tabBar.barTintColor = .red
@@ -36,6 +38,7 @@ class TabBarViewController: UITabBarController {
     func setupVCs() {
         viewControllers = [
             createNavController(for: stopWatchCoordinator.navigationController, title: NSLocalizedString("StopWatch", comment: ""), image: UIImage(systemName: "stopwatch.fill")!),
+            createNavController(for: alarmWatchCoordinator.navigationController, title: NSLocalizedString("Alarm", comment: ""), image: UIImage(systemName: "alarm.fill")!),
             createNavController(for: timerWatchCoordinator.navigationController, title: NSLocalizedString("Timer", comment: ""), image: UIImage(systemName: "timer.circle.fill")!)
         ]
     }
